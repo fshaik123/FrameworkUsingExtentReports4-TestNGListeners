6956e27fbb016c6f0c1b81690608365b793aee26
Shaik Fareed <fshaikautomation@gmail.com>
2020-05-15 13:48:01 +0530

    Extent Reports 4 using TestNG Listeners

--- src/test/java/com/framework/tests/GoogleSearchTests.java
+++ src/test/java/com/framework/tests/GoogleSearchTests.java
@@ -10,17 +10,16 @@ import java.io.IOException;
 public class GoogleSearchTests extends Reporting {
     @Test
     public void GoogleSearchTest1() throws IOException {
-        //logger = extent.createTest("GoogleSearchTest1");
+
         driver.navigate().to(getAppURL());
         HomePage homePage = new HomePage(driver);
-        Reporting reporting = new Reporting();
         homePage.typeTextInTheSearchField("excel in testing");
 
         String title = driver.getTitle();
         //logger.createNode("Verify Title");
         Assert.assertEquals(title, "excel in testing - Google Search");
         homePage.getResultsCount();
-        reporting.failureReport(driver(), "Test", "Test Failed");
+        //reporting.failureReport(driver(), "Test", "Test Failed");
         //logger.createNode("Verify Results count is displayed");
     }
 }
